c990a13796ac3ef3fd499c4c93305f128173523e
Karen Leonardo <ana.leonardo@lc.cuny.edu>
2023-12-18 22:39:54 -0500

    Added chat props to chatsPage for client

--- client/src/ChatsPage.jsx
+++ client/src/ChatsPage.jsx
@@ -1,5 +1,19 @@
+import {MultiChatSocket, MultiChatWindow, useMultiChatLogic} from 'react-chat-engine-advanced'
+
 const ChatsPage = ()=>{
-    return <>chats...</>
+    const chatProps = useMultiChatLogic(
+        '7d0e011f-0858-4e2b-a6ed-0c49880aadcc', 
+        props.user.username, 
+        props.user.secret
+    );
+    return(
+        <div style={ {height: '100vh'}}>
+            <MultiChatSocket{...chatProps}/>
+            <MultiChatWindow{...chatProps} style ={{height: '100%'}}/>
+
+        </div>
+    )
 }
 
 export default ChatsPage
+
