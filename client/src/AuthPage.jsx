c7d048890cac42d5ac8a3530f5cca384a271f691
Karen Leonardo <ana.leonardo@lc.cuny.edu>
2023-12-19 00:33:42 -0500

    Added more info to the front of the client page

--- client/src/AuthPage.jsx
+++ client/src/AuthPage.jsx
@@ -16,9 +16,18 @@ const AuthPage = (props) => {
   };
 
   return (
+  
     <div className="background">
+    <div className="intro">
+       <p >Hello! This is Ana Leonardo.</p>
+        <p>This is my final project for CMP 464.</p>
+        <p>I was inspired by a YouTube video where </p>
+           <p>a chat engine was used to make a messaging application.</p>
+           <p>My future goal is to be able to make it like Instagram AI</p>
+           <p>or Snapchat AI.</p>
+    </div>
 
-<img id="pic1" src={hImage} alt="Welcome to Chat.AI" />
+    <img id="pic1" src={hImage} alt="Welcome to Chat.AI" />
       <form onSubmit={onSubmit} className="form-card">
         <div className="form-title">Welcome to Chat.AI</div>
 
@@ -30,6 +39,12 @@ const AuthPage = (props) => {
           <button className="auth-button" type="submit">
             Enter
           </button>
+
+          </div>
+        {/* Add Instagram link */}
+        <div className="socail-link">
+          YouTube: <a href="https://youtu.be/Fzv-rgwcFKk?si=HDO2yKWjM9ijvjIV" target="_blank" rel="noopener noreferrer">@Realtime Chat</a>
+
         </div>
       </form>
     </div>
