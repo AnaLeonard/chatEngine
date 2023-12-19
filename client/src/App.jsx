bf44e2e9b82d2b9574f68e13b15173078b1e1787
Karen Leonardo <ana.leonardo@lc.cuny.edu>
2023-12-18 23:56:24 -0500

    Trouble shooting image

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,31 +1,23 @@
+import { useState } from "react";
 
+import "./App.css";
 
-
-import React, { useState } from "react";
 import AuthPage from "./AuthPage.jsx";
 import ChatsPage from "./ChatsPage";
-import "./App.css"; // Import your styles
 
 function App() {
   const [user, setUser] = useState(undefined);
 
   // if the user exists return user otherwise make one
   if (!user) {
-    return (
-      <div className="container">
-        <img src="/assets/h.jpeg"  />
-        <AuthPage onAuth={(user) => setUser(user)} />
-      </div>
-    );
+    return <AuthPage onAuth={(user) => setUser(user)} />;
   } else {
-    return (
-      <div className="container">
-        <img src="/assets/h.jpeg"  />
-        <ChatsPage user={user} />
-      </div>
-    );
+    return <ChatsPage user={user} />;
   }
 }
 
 export default App;
+
+
+
 
--- client/src/AuthPage.jsx
+++ client/src/AuthPage.jsx
@@ -1,35 +1,37 @@
-import axios from 'axios'
+import axios from 'axios';
 
 const AuthPage = (props) => {
-    const onSubmit = (e) => {
-      e.preventDefault();
-      const { value } = e.target[0];
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const { value } = e.target.username;
 
-      axios.post(
-        'http://localhost:3001/authenticate',
-        {username: value}
-      )
-      .then( r => props.onAuth({...r.data, secret: value}))
-      .catch(e => console.log('error', e))
-    };
-  //forms that takes in username
-    return (
-      <div className="background">
-        <form onSubmit={onSubmit} className="form-card">
-          <div className="form-title">Welcome 👋</div>
-  
-          <div className="form-subtitle">Set a username to get started</div>
-  
-          <div className="auth">
-            <div className="auth-label">Username</div>
-            <input className="auth-input" name="username" />
-            <button className="auth-button" type="submit">
-              Enter
-            </button>
-          </div>
-        </form>
-      </div>
-    );
+    axios.post(
+      'http://localhost:3001/authenticate',
+      { username: value }
+    )
+      .then((response) => props.onAuth({ ...response.data, secret: value }))
+      .catch((error) => console.log('error', error));
   };
-  
-  export default AuthPage;
+
+  return (
+    <div className="background">
+
+<img id="pic1" src="/h.jpeg" alt="Welcome Image" />
+      <form onSubmit={onSubmit} className="form-card">
+        <div className="form-title">Welcome to Chat.AI</div>
+
+        <div className="form-subtitle">Set a username to get started</div>
+
+        <div className="auth">
+          <div className="auth-label">Username</div>
+          <input className="auth-input" name="username" id="username" />
+          <button className="auth-button" type="submit">
+            Enter
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default AuthPage;
